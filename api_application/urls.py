0264f79e831e98190da01bc6265d3f91a18b44bd
seohanta <simpletop4ik@gmail.com>
2025-12-07 09:11:39 +0300

    Update urls.py

--- api_application/urls.py
+++ api_application/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
+# главный url файл
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.core.urls')),
